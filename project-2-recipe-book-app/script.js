eda8071c5e205a1f81c9a94ca51c76c486fe5e43
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-05 02:46:06 +0600

    Update script.js

--- project-2-recipe-book-app/script.js
+++ project-2-recipe-book-app/script.js
@@ -1,6 +1,40 @@
-
-
 const API_KEY = "6716bbd7e07b44498e4287907d91a5ca";
+
+const recipeListElements = document.getElementById('recipe-list');
+
+function displayRecipes(recipes) {
+    recipeListElements.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
+
+        const recipeImageEl = document.createElement("img");
+        recipeImageEl.src = recipe.image;
+        recipeImageEl.alt = "recipe";
+
+        const recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText = recipe.title;
+
+        recipeIngredientsEl = document.createElement("p");
+        recipeIngredientsEl.innerHTML = `
+            <strong>Ingredients:</strong> ${recipe.extendedIngredients.map((ingredient) => ingredient.original).join(", ")}
+        `;
+
+        const recipeLinkEl = document.createElement("a");
+        recipeLinkEl.href = recipe.sourceUrl;
+        recipeLinkEl.innerText = "View Recipe";
+        
+        recipeItemEl.appendChild(recipeImageEl);
+
+        recipeItemEl.appendChild(recipeTitleEl);
+        
+        recipeItemEl.appendChild(recipeIngredientsEl);
+
+        recipeItemEl.appendChild(recipeLinkEl);
+
+        recipeListElements.appendChild(recipeItemEl);
+    });
+}
 
 async function getRecipes() {
     const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`)
@@ -13,7 +47,8 @@ async function getRecipes() {
 async function init() {
     const recipes = await getRecipes()
 
-    console.log(recipes);
+    // console.log(recipes);
+    displayRecipes(recipes);
 }
 
 init()
