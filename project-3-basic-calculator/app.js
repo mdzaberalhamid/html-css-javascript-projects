8d0f12a2d72a182da7e34e62d6583c51379d29d3
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-09 01:53:52 +0600

    Update app.js

--- project-3-basic-calculator/app.js
+++ project-3-basic-calculator/app.js
@@ -1,28 +1,32 @@
 const buttonsElement = document.querySelectorAll('button');
 
+const inputField = document.getElementById("result");
+
 // console.log(buttonsElement);
 for (let i = 0; i < buttonsElement.length; i++) {
     buttonsElement[i].addEventListener('click', ()=> {
+
         // console.log(buttonsElement[i].textContent);
         const buttonValue = buttonsElement[i].textContent;
+
         if (buttonValue === "C") {
             clearResult();
         } else if (buttonValue === "=") {
             calculateResult();
         } else {
-            appendValue();
+            appendValue(buttonValue);
         }
     })
 }
 
 function clearResult () {
-
+    inputField.value = "";
 }
 
 function calculateResult () {
-
+    inputField.value = eval(inputField.value);
 }
 
-function appendValue () {
-    
+function appendValue (buttonValue) {
+    inputField.value += buttonValue;
 }
