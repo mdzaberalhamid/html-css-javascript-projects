7fbe1567cfc2687df9c04416630577db863dd12c
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-14 20:07:13 +0600

    Update main.js

--- project-4-pomodoro-timer/main.js
+++ project-4-pomodoro-timer/main.js
@@ -3,16 +3,43 @@ const stopBtn = document.getElementById("stop");
 const resetBtn = document.getElementById("reset");
 const timer = document.getElementById("timer");
 
+let interval;
+let timeLeft = 10;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    // let formattedTime = minutes + ":" + seconds;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    timer.innerHTML = formattedTime;
+}
+
 function startTimer() {
-    console.log('Start!');
+    // console.log('Start!');
+    interval = setInterval(()=>{
+        // console.log('test');
+        timeLeft--;
+        updateTimer();
+        if (timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 60;
+        }
+    }, 1000);
 }
 
 function stopTimer() {
-    console.log('Stop.');
+    // console.log('Stop.');
+    clearInterval(interval);
 }
 
 function resetTimer() {
-    console.log('Reset.');
+    // console.log('Reset.');
+    clearInterval(interval);
+    timeLeft = 60;
+    updateTimer();
 }
 
 startBtn.addEventListener("click", startTimer);
