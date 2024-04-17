7d623a8d47bec9c184e3d1f3f76cb5e1c25e5c98
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-17 23:06:32 +0600

    Add files via upload

---
+++ project-5-currency-converter/main.js
@@ -0,0 +1,11 @@
+const currencyFirstEl = document.getElementById("currency-first");
+
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+// Continuing...
