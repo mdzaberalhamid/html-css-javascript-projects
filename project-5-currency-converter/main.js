acdc6455db890d3c02d30e42285e47f44678d054
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-18 23:13:54 +0600

    Update main.js

--- project-5-currency-converter/main.js
+++ project-5-currency-converter/main.js
@@ -8,4 +8,14 @@ const worthSecondEl = document.getElementById("worth-second");
 
 const exchangeRateEl = document.getElementById("exchange-rate");
 
+function updateRate () {
+    console.log("Option changed!");
+}
+
+currencyFirstEl.addEventListener("change", updateRate)
+
+currencySecondEl.addEventListener("change", updateRate)
+
+worthFirstEl.addEventListener("input", updateRate)
+
 // Continuing...
