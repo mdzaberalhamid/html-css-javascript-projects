38a86f428d541206ce5892887fa50475c65545b9
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-19 23:27:00 +0600

    Update main.js

--- project-5-currency-converter/main.js
+++ project-5-currency-converter/main.js
@@ -8,14 +8,29 @@ const worthSecondEl = document.getElementById("worth-second");
 
 const exchangeRateEl = document.getElementById("exchange-rate");
 
-function updateRate () {
-    console.log("Option changed!");
+updateRate();
+
+function updateRate() {
+  // console.log("Option changed!");
+  // fetch(`https://v6.exchangerate-api.com/v6/5f9d1c87f7250159c9c9b17d/latest/${currencyFirstEl.value}`).then((res) => res.json()).then((data) => console.log(data))
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/2c3b1321a42ead76f059e5cf/latest/${currencyFirstEl.value}`
+  )
+    .then((res) => res.json())
+    // .then((data) => console.log(data))
+    .then((data) => {
+      const rate = data.conversion_rates[currencySecondEl.value];
+      // console.log(rate);
+      exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${
+        rate + " " + currencySecondEl.value
+      }`;
+      worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2)
+    });
 }
 
-currencyFirstEl.addEventListener("change", updateRate)
+currencyFirstEl.addEventListener("change", updateRate);
 
-currencySecondEl.addEventListener("change", updateRate)
+currencySecondEl.addEventListener("change", updateRate);
 
-worthFirstEl.addEventListener("input", updateRate)
-
-// Continuing...
+worthFirstEl.addEventListener("input", updateRate);
