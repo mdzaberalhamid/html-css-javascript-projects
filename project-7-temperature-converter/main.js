7ac9ea5be3f5763058583c28a429952d1fe68b09
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-05-16 23:37:03 +0600

    Update main.js

--- project-7-temperature-converter/main.js
+++ project-7-temperature-converter/main.js
@@ -1,5 +1,24 @@
 
 
-function computeTemp () {
-    console.log('Value changed!');
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
+function computeTemp (event) {
+    // console.log('Value changed!');
+    // console.log(event.target.value);
+
+    const currentValue = +event.target.value;
+
+    switch (event.target.name) {
+        case "celsius":
+            kelvinEl.value = (currentValue + 273.32);
+            fahrenheitEl.value = (currentValue * 1.8 + 32);
+            break;
+    
+        default:
+            break;
+    }
+
+    // console.log(fahrenheitEl.value);
 }
