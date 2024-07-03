5dd6128901ef29f8af4fd3aa00327520c425ef59
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-07-03 22:31:54 +0600

    Update main.js

--- project-9-dice-roll-simulator/main.js
+++ project-9-dice-roll-simulator/main.js
@@ -1,13 +1,37 @@
-// ----------
 
 const buttonEl = document.getElementById("roll-button");
 
 const diceEl = document.getElementById("dice");
+
+function rollDice() {
+    const rollResult = Math.floor(Math.random() * 6) + 1;
+    // console.log(rollResult);
+    const rollFace = getDiceFace(rollResult);
+    console.log(rollFace);
+}
+
+function getDiceFace(rollResult){
+    switch(rollResult){
+        case 1:
+            return "&#9856";
+        case 2:
+            return "&#9857";
+        case 3:
+            return "&#9858";
+        case 4:
+            return "&#9859";
+        case 5:
+            return "&#9860";
+        case 6:
+            return "&#9861";
+    }
+}
 
 buttonEl.addEventListener("click", ()=> {
     // console.log("Clicked!");
     diceEl.classList.add("roll-animation");
     setTimeout( () => {
         diceEl.classList.remove("roll-animation");
+        rollDice();
     }, 1000);
 })
