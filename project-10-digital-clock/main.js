2c96046bf1d4047c0b46d72bbe4f07cbf5fa3528
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-07-11 22:52:05 +0600

    Add files via upload

---
+++ project-10-digital-clock/main.js
@@ -0,0 +1,8 @@
+
+const hourEl = document.getElementById("hours");
+
+const minuteEl = document.getElementById("minutes");
+
+const secondEl = document.getElementById("seconds");
+
+const ampmEl = document.getElementById("am-pm");
